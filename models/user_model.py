22b88eb0b53b95db1d81a0f72e1708eb68f7ee9f
Emmar Caber <caberemmar@gmail.com>
2023-04-13 23:27:08 +0800

    feat: connect the user_model to db

--- models/user_model.py
+++ models/user_model.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,44 +15,60 @@ class User(Base):
     password = Column(String)
 
 class UserDatabase:
-    def __init__(self, db_uri='sqlite:///example.db'):
+    def __init__(self, db_path='database/kiosk.db'):
+        os.makedirs(os.path.dirname(db_path), exist_ok=True)    # if file does not exist, create the file
+        db_uri = 'sqlite:///' + db_path                         # merge the path to create a uri
         engine = create_engine(db_uri)
         Base.metadata.create_all(engine)
         self.Session = sessionmaker(bind=engine)
 
-    def create_user(self, first_name, last_name, id_number, password):
+
+
+    def create_user(self, id_number, first_name, last_name, password):
+        """Create user account
+
+        Args:
+            id_number (string): user id_number
+            first_name (string): user first_name
+            last_name (string): user last_name
+            password (string): user password
+
+        Returns:
+            object: instance of a user
+        """
         session = self.Session()
-        user = User(first_name=first_name, last_name=last_name, id_number=id_number, password=password)
+        user = User(id_number=id_number, first_name=first_name, last_name=last_name, password=password)
         session.add(user)
         session.commit()
         session.close()
         return user
 
-    def read_user(self, id):
+    # Function to read user
+    def read_user(self, id_number):
         session = self.Session()
-        user = session.query(User).filter(User.id == id).first()
+        user = session.query(User).filter(User.id_number == id_number).first()
         session.close()
         return user
 
-    def update_user(self, id, first_name=None, last_name=None, id_number=None, password=None):
+
+    # 
+    def update_user(self, id_number, first_name=None, last_name=None, password=None):
         session = self.Session()
-        user = session.query(User).filter(User.id == id).first()
+        user = session.query(User).filter(User.id_number == id_number).first()
         if user:
             if first_name:
                 user.first_name = first_name
             if last_name:
                 user.last_name = last_name
-            if id_number:
-                user.id_number = id_number
             if password:
                 user.password = password
             session.commit()
         session.close()
         return user
 
-    def delete_user(self, id):
+    def delete_user(self, id_number):
         session = self.Session()
-        user = session.query(User).filter(User.id == id).first()
+        user = session.query(User).filter(User.id_number == id_number).first()
         if user:
             session.delete(user)
             session.commit()
