34323efc07095336c45c1d1c4a08b6484a76d00f
Emmar Caber <caberemmar@gmail.com>
2023-11-27 14:45:06 +0800

    feat: add the format_question_and_option function in other subcategories

--- app/views/eeps_subcategories_view.py
+++ app/views/eeps_subcategories_view.py
@@ -1,8 +1,15 @@
 from PySide6.QtWidgets import QMainWindow
 from ..uis.ui_eeps_subcategories import Ui_EEPSWindow
+from app.views.randomize_view import RandomizeWindow
+
+from app.models.question import Question
+from app.models.option import Option
+
+import random
+
 
 class EEPSWindow(QMainWindow, Ui_EEPSWindow):
-    def __init__(self, student_name, categories_window = None):
+    def __init__(self, student_name, categories_window=None):
         super().__init__()
 
         self.setupUi(self)
@@ -18,81 +25,115 @@ class EEPSWindow(QMainWindow, Ui_EEPSWindow):
         self.btnBack.clicked.connect(self.back_to_categories_window)
 
         self.btnElectricCircuits.clicked.connect(self.generate_electricCircuits)
-        self.btnElectronicTheoryCircuits.clicked.connect(self.generate_electronicTheoryCircuits)
+        self.btnElectronicTheoryCircuits.clicked.connect(
+            self.generate_electronicTheoryCircuits
+        )
         self.btnEnergyConversion.clicked.connect(self.generate_energyConversion)
-        self.btnPowerTransmissionDistribution.clicked.connect(self.generate_powerTransmissionDistribution)
-        self.btnInstrumentationMeasurement.clicked.connect(self.generate_instrumentationMeasurement)
-        self.btnCircuitLineProtection.clicked.connect(self.generate_circuitLineProtection)
+        self.btnPowerTransmissionDistribution.clicked.connect(
+            self.generate_powerTransmissionDistribution
+        )
+        self.btnInstrumentationMeasurement.clicked.connect(
+            self.generate_instrumentationMeasurement
+        )
+        self.btnCircuitLineProtection.clicked.connect(
+            self.generate_circuitLineProtection
+        )
         self.btnControlSystem.clicked.connect(self.generate_controlSystem)
-        self.btnPrinciplesCommunication.clicked.connect(self.generate_principlesCommunication)
+        self.btnPrinciplesCommunication.clicked.connect(
+            self.generate_principlesCommunication
+        )
         self.btnComponentsDevices.clicked.connect(self.generate_componentsDevices)
         self.btnElectricalMachines.clicked.connect(self.generate_electricalMachines)
         self.btnPowerPlant.clicked.connect(self.generate_powerPlant)
         self.btnIllumination.clicked.connect(self.generate_illumination)
         self.btnBuildingWiring.clicked.connect(self.generate_buildingWiring)
-        self.btnElectronicPowerEquipment.clicked.connect(self.generate_electronicPowerEquipment)
+        self.btnElectronicPowerEquipment.clicked.connect(
+            self.generate_electronicPowerEquipment
+        )
         self.btnElectricSystems.clicked.connect(self.generate_electricSystems)
 
+    def format_questions_and_options(self, subcategory_name):
+        # Get the questions and options from db
+        questions = Question.get_questions_by_subcategories(subcategory_name)
+        options = Option.get_options_by_subcategories(subcategory_name)
+
+        # Initialize an empty dict that will hold questions
+        formatted_questions = {}
+
+        # Get all the questions first
+        for question in questions:
+            question_text = question["text"]
+            question_id = question["id"]
+            correct_option = question["correct_option"]
+
+            question_options = {}
+
+            # Iterate through all the options
+            # and if matches the questionId, include it
+            for option in options:
+                if option["questionId"] == question_id:
+                    question_options[option["option"]] = option["text"]
+
+            # Finalize and format the questions
+            formatted_questions[question_text] = {
+                "options": question_options,
+                "correct_option": correct_option,
+            }
+
+        # Select random 20 questions from DB without replicates
+        selected_questions = random.sample(list(formatted_questions.keys()), 20)
+
+        # Shuffle the formatted questions based on selected random 20 questions
+        shuffle_formatted_questions = {
+            key: formatted_questions[key] for key in selected_questions
+        }
+
+        return shuffle_formatted_questions
 
     def generate_electricCircuits(self):
         print("Electric Circuits")
 
-
     def generate_electronicTheoryCircuits(self):
         print("Electronic Theory Circuits")
-
 
     def generate_energyConversion(self):
         print("Energy Conversion")
 
-
     def generate_powerTransmissionDistribution(self):
         print("Power Transmission and Distribution")
-
 
     def generate_instrumentationMeasurement(self):
         print("Instrumentation and Measurement ")
 
-
     def generate_circuitLineProtection(self):
         print("Circuit Line and Protection")
-
 
     def generate_controlSystem(self):
         print("Control System")
 
-
     def generate_principlesCommunication(self):
         print("Principles of Communication")
-
 
     def generate_componentsDevices(self):
         print("Components and Devices")
 
-
     def generate_electricalMachines(self):
         print("Electrical Machines")
-
 
     def generate_powerPlant(self):
         print("Power Plant")
 
-
     def generate_illumination(self):
         print("Illumination")
-
 
     def generate_buildingWiring(self):
         print("Building Wiring")
 
-
     def generate_electronicPowerEquipment(self):
         print("Electronic Power Equipment")
 
-
     def generate_electricSystems(self):
         print("Electric Systems")
-
 
     def back_to_categories_window(self):
         if self.back_to_categories_window:
--- app/views/math_subcategories_view.py
+++ app/views/math_subcategories_view.py
@@ -1,8 +1,15 @@
 from PySide6.QtWidgets import QMainWindow
 from ..uis.ui_math_subcategories import Ui_MathWindow
+from app.views.randomize_view import RandomizeWindow
+
+from app.models.question import Question
+from app.models.option import Option
+
+import random
+
 
 class MathWindow(QMainWindow, Ui_MathWindow):
-    def __init__(self, student_name, categories_window = None):
+    def __init__(self, student_name, categories_window=None):
         super().__init__()
 
         self.setupUi(self)
@@ -21,59 +28,89 @@ class MathWindow(QMainWindow, Ui_MathWindow):
         self.btnTrigonometry.clicked.connect(self.generate_trigonometry)
         self.btnAnalyticalGeometry.clicked.connect(self.generate_analyticalGeometry)
         self.btnDifferentialCalculus.clicked.connect(self.generate_differentialCalculus)
-        self.btnDifferentialEquations.clicked.connect(self.generate_differentialEquations)
+        self.btnDifferentialEquations.clicked.connect(
+            self.generate_differentialEquations
+        )
         self.btnIntegralCalculus.clicked.connect(self.generate_integralCalculus)
         self.btnComplexNumbers.clicked.connect(self.generate_complexNumbers)
         self.btnMatrices.clicked.connect(self.generate_matrices)
         self.btnPowerSeries.clicked.connect(self.generate_powerSeries)
         self.btnFourierAnalysis.clicked.connect(self.generate_fourierAnalysis)
         self.btnLaplaceTransforms.clicked.connect(self.generate_laplaceTransforms)
-        self.btnProbabilityStatistics.clicked.connect(self.generate_probabilityStatistics)
+        self.btnProbabilityStatistics.clicked.connect(
+            self.generate_probabilityStatistics
+        )
 
+    def format_questions_and_options(self, subcategory_name):
+        # Get the questions and options from db
+        questions = Question.get_questions_by_subcategories(subcategory_name)
+        options = Option.get_options_by_subcategories(subcategory_name)
+
+        # Initialize an empty dict that will hold questions
+        formatted_questions = {}
+
+        # Get all the questions first
+        for question in questions:
+            question_text = question["text"]
+            question_id = question["id"]
+            correct_option = question["correct_option"]
+
+            question_options = {}
+
+            # Iterate through all the options
+            # and if matches the questionId, include it
+            for option in options:
+                if option["questionId"] == question_id:
+                    question_options[option["option"]] = option["text"]
+
+            # Finalize and format the questions
+            formatted_questions[question_text] = {
+                "options": question_options,
+                "correct_option": correct_option,
+            }
+
+        # Select random 20 questions from DB without replicates
+        selected_questions = random.sample(list(formatted_questions.keys()), 20)
+
+        # Shuffle the formatted questions based on selected random 20 questions
+        shuffle_formatted_questions = {
+            key: formatted_questions[key] for key in selected_questions
+        }
+
+        return shuffle_formatted_questions
 
     def generate_algebra(self):
         print("Algebra")
 
-
     def generate_trigonometry(self):
         print("Trigonometry")
-
 
     def generate_analyticalGeometry(self):
         print("Analytical Geometry")
 
-
     def generate_differentialCalculus(self):
         print("Differential Calculus")
-
 
     def generate_differentialEquations(self):
         print("Differential Equations")
 
-
     def generate_integralCalculus(self):
         print("Integral Calculus")
-
 
     def generate_complexNumbers(self):
         print("Complex Numbers")
 
-
     def generate_matrices(self):
         print("Matrices")
-
 
     def generate_powerSeries(self):
         print("Power Series")
 
-
     def generate_fourierAnalysis(self):
         print("Fourier Analysis")
 
-
     def generate_laplaceTransforms(self):
         print("Laplace Transforms")
-
 
     def generate_probabilityStatistics(self):
         print("Probability and Statistics")
