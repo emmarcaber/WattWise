bd2331d1d474d5cf47e94d25967b41b6b3fee98d
Emmar Caber <caberemmar@gmail.com>
2023-12-06 09:41:29 +0800

    feat: finish the check module

--- app/views/check_view.py
+++ app/views/check_view.py
@@ -3,6 +3,8 @@ from PySide6.QtCore import Qt, QTimer, Slot
 from PySide6.QtGui import QImage, QPixmap
 
 from ..uis.ui_check import Ui_CheckWindow
+from app.controllers.check_controller import CheckController
+from app.models.test import Test
 from app.views.main_menu_view import MainMenu
 
 import cv2
@@ -16,6 +18,9 @@ class CheckWindow(QMainWindow, Ui_CheckWindow):
         self.student_name = student_name
         self.student_id = student_id
         self.main_menu_window = main_menu_window
+        self.check_controller = CheckController()
+        self.check_controller.create_paths()
+        self.test = Test()
 
         self.setupUi(self)
         self.setWindowTitle(f"WattWise | Check")
@@ -30,6 +35,8 @@ class CheckWindow(QMainWindow, Ui_CheckWindow):
     def modifyWindow(self):
         self.btnBackMainMenu.clicked.connect(self.back_to_main_menu)
         self.btnCapture.clicked.connect(self.capture_image)
+        self.btnCheck.clicked.connect(self.check_answer_sheet)
+        self.btnProfile.clicked.connect(self.open_profile_window)
         self.integrate_camera_to_frame()
 
     def integrate_camera_to_frame(self):
@@ -62,6 +69,119 @@ class CheckWindow(QMainWindow, Ui_CheckWindow):
             pixmap = QPixmap.fromImage(q_image)
             self.video_label.setPixmap(pixmap)
 
+    def validate_test_id(self, test_id):
+        if self.test.is_test_checked(test_id):
+            self.error_validation("The answer sheet has been already checked!")
+        elif not self.test.test_belong_to_current_user(test_id, self.student_id):
+            self.error_validation(
+                "The answer sheet does not belong to the current user!"
+            )
+        else:
+            self.success_validation()
+
+    def error_validation(self, text):
+        msg = self.message_box_creator(text, True)
+        msg.exec()
+
+        self.back_to_main_menu()
+
+    def success_validation(self):
+        # Disable the capture button after one click
+        self.btnCapture.setEnabled(False)
+
+        # Change its appearance
+        self.btnCapture.setStyleSheet(
+            """
+            QPushButton {
+                background-color: #DDDDDD;
+            }
+            """
+        )
+
+        msg = self.message_box_creator(
+            f"The answer sheet with Test ID of {self.test_id} has been successfully captured!",
+            False,
+        )
+
+        msg.exec()
+
+        self.setup_answers()
+
+    def setup_answers(self):
+        self.answers = self.check_controller.get_answers_from_captured_answer_sheet()
+
+        self.label_items = [
+            self.labelItem1,
+            self.labelItem2,
+            self.labelItem3,
+            self.labelItem4,
+            self.labelItem5,
+            self.labelItem6,
+            self.labelItem7,
+            self.labelItem8,
+            self.labelItem9,
+            self.labelItem10,
+            self.labelItem11,
+            self.labelItem12,
+            self.labelItem13,
+            self.labelItem14,
+            self.labelItem15,
+            self.labelItem16,
+            self.labelItem17,
+            self.labelItem18,
+            self.labelItem19,
+            self.labelItem20,
+        ]
+
+        count = 1
+        for label, answer in zip(self.label_items, self.answers):
+            label.setText(f"{count}. {answer}")
+            count += 1
+
+    def check_answer_sheet(self):
+        correct_answers_str = self.test.get_correct_answers_of_test_from_db(
+            self.test_id
+        )
+        self.correct_answers = [
+            element.strip()
+            for element in correct_answers_str.strip(",").split(",")
+            if element.strip()
+        ]
+
+        self.show_score()
+
+    def show_score(self):
+        score = 0
+        for label, (answer, correct_answer) in zip(
+            self.label_items, zip(self.answers, self.correct_answers)
+        ):
+            if answer == correct_answer:
+                label.setStyleSheet(
+                    "background-color: #04AA6D; color: white; padding-left: 3px"
+                )
+                score += 1
+            else:
+                label.setStyleSheet(
+                    "background-color: #B22222; color: white; padding-left: 3px"
+                )
+
+        self.labelResults.setText(f"{score}/20")
+
+        # Update the tests table in the database
+        self.test.update_score_of_tests_in_db(score, self.test_id)
+
+        # Disable the capture button after one click
+        self.btnCheck.setEnabled(False)
+
+        # Change its appearance
+        self.btnCheck.setStyleSheet(
+            """
+            QPushButton {
+                background-color: #DDDDDD;
+            }
+            """
+        )
+
     def capture_image(self):
         ret, frame = self.video_capture.read()
 
@@ -74,28 +194,14 @@ class CheckWindow(QMainWindow, Ui_CheckWindow):
                 )
             )
 
-            cv2.imwrite(image_path, frame)  # Save the captured image
+            # cv2.imwrite(image_path, frame)  # Save the captured image
             self.timer.stop()  # Stop the timer (camera feed)
 
             # Release the video capture
             self.video_capture.release()
 
-        msg = self.message_box_creator(
-            "The answer sheet was successfully captured!", False
-        )
-        msg.exec()
-
-        # Disable the capture button after one click
-        self.btnCapture.setEnabled(False)
-
-        # Change its appearance
-        self.btnCapture.setStyleSheet(
-            """
-        QPushButton {
-            background-color: #DDDDDD;
-        }
-            """
-        )
+        self.test_id = self.check_controller.scan_qrcode()
+        self.validate_test_id(self.test_id)
 
     def closeCamera(self):
         # Release the video capture when the application is closed
@@ -108,6 +214,15 @@ class CheckWindow(QMainWindow, Ui_CheckWindow):
             self.student_id,
         )
         self.main_menu_window.show()
+        self.hide()
+
+    def open_profile_window(self):
+        from app.views.profile_view import ProfileWindow
+
+        self.profile_window = ProfileWindow(
+            self.student_name, self.student_id, main_menu_window=self.main_menu_window
+        )
+        self.profile_window.show()
         self.hide()
 
     def message_box_creator(self, text, is_error):
